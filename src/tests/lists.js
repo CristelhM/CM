533915d815604ba7967b2591e579e29360ba745e
Cristelh <Cristelh.Miranda@abila.com>
2017-02-28 22:34:44 -0800

    added acceptance test.

---
+++ src/tests/lists.js
@@ -0,0 +1,34 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+
+// These are Chimp globals
+/* globals browser assert server */
+
+function countLists() {
+  browser.waitForExist('.list-todo');
+  const elements = browser.elements('.list-todo');
+  return elements.value.length;
+};
+
+function signinExists() {
+  browser.waitForExist('.login-link-text');
+  const elements = browser.elements('.login-link-text');
+  return elements.value.length > 0;
+}
+
+describe('list ui', function () {
+  beforeEach(function () {
+    browser.url('http://localhost:3000');
+    //server.call('generateFixtures');
+  });
+
+  it('can sign up @watch', function () {
+    //const initialCount = countLists();
+
+    //browser.click('.js-new-list');
+
+    //assert.equal(countLists(), initialCount + 1);
+    const hasSignUp = signinExists();
+    assert.equal(hasSignUp, true);
+  });
+});
